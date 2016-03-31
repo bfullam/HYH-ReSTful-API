d92f1b0c07fffcc49c0707b952fcd33573e7f2cc
Bryan Fullam <bryanfullam@gmail.com>
2016-03-30 17:12:00 -0700

    guh
    ugh ugh

--- restful.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request #import objects from the flask model
-app = Flask(_name_) #define app using flask
-
-@app.route('/', methods=['GET'])
-def test():
-    return jsonify({'message' : 'It works!'})
-
-if _name_ == '_main_':
-    app.run(debug=True, port=8000) #run app on port 8000 in debug mode
